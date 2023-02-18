f5de65fa70d6c3a15fc154c8186cc557fa8661db
GeloMmadi <122824022+GeloMmadi@users.noreply.github.com>
2023-02-18 16:28:14 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - a program that print a line using a printf function
- * Return: 0 (succeess)
+ *  *main - Entry point
+ *   *My hello, printf task
+ *    *Return: return 1 after program execution
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-			return (0);
-		}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
