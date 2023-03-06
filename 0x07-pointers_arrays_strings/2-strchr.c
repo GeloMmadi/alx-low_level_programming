4c8ba63e214cfe87e204215ba966e197de2d7ae6
gelommadi <mmadigole@gmail.com>
2023-03-06 12:40:11 -0800

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,21 @@
 #include "main.h"
-/**
- * *_strchr - Returns a pointer to the first occurrence
- * *of the character c in the string s, or NULL if the
- * *character is not found
- * *
- * *@s:string targeted
- * *@c:character targeted
- * *
- * *Return: returns pointer to first occcurence of c
- */
-char *_strchr(char *s, char c)
-{
-int i;
+#include <stdio.h>
 
-for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-						;
-if (s[i] == c)
-return (s + i);
-else
-return (NULL);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
 }
